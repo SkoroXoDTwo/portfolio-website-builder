8fd6f0cb44308bde42d62dd6bbf921471c22c421
Alexey Belkov <lexabelco2013@yandex.ru>
2023-02-15 22:25:05 +0500

    feat: подключил шрифты и файл нормализации стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import "./vendor/normalize.scss";
+import "./vendor/fonts.scss";
+import "./index.scss";
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
