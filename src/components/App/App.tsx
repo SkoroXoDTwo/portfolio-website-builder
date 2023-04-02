9b25929f30cd54304769d79f0858bc943c295fee
Alexey Belkov <lexabelco2013@yandex.ru>
2023-04-02 23:25:16 +0500

    feat: создать компонент signin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,12 +8,14 @@ import {
 import Projects from '../../pages/Projects';
 import About from '../../pages/About';
 import Technologies from '../../pages/Technologies';
+import Signin from '../../pages/Signin';
 
 const App = () => {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Projects />} />
+        <Route path='/sign-in' element={<Signin />} />
         <Route path="/about" element={<About />} />
         <Route path="/technologies" element={<Technologies />} />
       </Routes>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,11 +1,12 @@
 import { FunctionComponent } from 'react';
 import Main from '../components/Main/Main';
+import SigninForm from '../components/SigninForm/SigninForm';
 
 const Signin: FunctionComponent = () => {
   return (
     <>
       <Main>
-
+        <SigninForm />
       </Main>
     </>
   );
